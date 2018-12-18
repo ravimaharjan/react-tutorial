7084add0742c00b99ee6b667e6310c9e417bde45
Ravi Maharjan <ravi_maharjan@logpoint.com>
2018-12-18 21:53:03 +0545

    first app with create-react-app

---
+++ myapp/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <h1>My first React App!</h1>
+      <p>Welcome.</p>
+      <Ninjas/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ myapp/src/Ninjas.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Ninjas extends Component{
+    render(){
+        return(
+            <div className="ninjas">
+                <div>Name: Ravi</div>
+                <div>Age: 30</div>
+                <div>Belt: black</div>
+            </div>            
+        )
+    }
+}
+
+export default Ninjas
