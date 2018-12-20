ea6f9ebc4d8585972cc06f865da1ee8a6c5367f0
Ravi Maharjan <ravi_maharjan@logpoint.com>
2018-12-20 21:15:03 +0545

    multi props passed

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from 'react';
 import Ninjas from './Ninjas';
 class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ravi', age: 30, belt: 'black', id: 1},
+      { name: 'Rashna', age: 23, belt: 'black', id: 2},
+      { name: 'Kirshna', age: 33, belt: 'black', id: 3}
+    ]
+  }
+  
   render() {
     return (
       <div className="App">
       <h1>My first React App!</h1>
       <p>Welcome.</p>
-      <Ninjas name="Ravi" age="20" belt="black"/>
-      <Ninjas name="Manish" age="40" belt="black"/>
+      <Ninjas ninjas={ this.state.ninjas }/>
+      
       </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react'
 
 class Ninjas extends Component{
     render(){
-        const {name, age, belt} = this.props;
+        const { ninjas } = this.props;
+        const ninjasList = ninjas.map(ninja =>{
+            return (
+                <div className="ninjas" key={ ninja.id }>
+                    <div>Name: { ninja.name }</div>
+                    <div>Age: { ninja.age }</div>
+                    <div>Belt: { ninja.belt }</div>
+                </div> 
+            )
+        })
         return(
-            <div className="ninjas">
-                <div>Name: { name }</div>
-                <div>Age: { age }</div>
-                <div>Belt: { belt }</div>
-            </div>            
+            <div className="ninjasList">
+                { ninjasList }
+            </div>
         )
     }
 }
