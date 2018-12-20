5d5bb9eefa3a5b4d4b42f2b28e7391d1d796c487
Ravi Maharjan <ravi_maharjan@logpoint.com>
2018-12-20 21:48:03 +0545

    class changed to the stateless function in Ninja.js

---
+++ myapp/src/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Ninjas from './Ninjas';
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ravi', age: 30, belt: 'black', id: 1},
+      { name: 'Rashna', age: 23, belt: 'black', id: 2},
+      { name: 'Kirshna', age: 33, belt: 'black', id: 3}
+    ]
+  }
+  
+
+  
+  render() {
+    return (
+      <div className="App">
+      <h1>My first React App!</h1>
+      <p>Welcome.</p>
+      <Ninjas ninjas={ this.state.ninjas }/>
+      
+      </div>
+    );
+  }
+}
+
+export default App;
